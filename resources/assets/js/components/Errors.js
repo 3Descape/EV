f8d689da7e0302d80094c365bba3ce490f624fde
bg81lack <miclack30@gmail.com>
2017-12-24 12:53:34 +0100

    Add utility files

---
+++ resources/assets/js/components/Errors.js
@@ -0,0 +1,24 @@
+export default class Errors{
+    constructor(){
+        this.errors = {};
+    }
+
+    setErrors(errors){
+        this.errors = errors
+    }
+
+    clearErrors(){
+        this.errors = {}
+    }
+
+    getError(name){
+        return this.errors[name];
+    }
+
+    hasError(name){
+        if(this.errors.hasOwnProperty(name)){
+            return this.errors[name];
+        }
+        return null;
+    }
+}
---
+++ resources/assets/js/components/Message.vue
@@ -0,0 +1,54 @@
+<template>
+    <transition name="fade">
+        <div class="alert info" :class="messageClass" role="alert" v-show="show">
+            <i class="fa fa-info-circle"></i> {{message}}
+        </div>
+    </transition>
+</template>
+
+<script>
+import {EventBus} from './EventBus.js';
+
+export default {
+    data: ()=>{
+        return{
+            show: false,
+            message: "",
+            type: "success"
+        }
+    },
+    computed: {
+        messageClass: function(){
+            return 'alert-' + this.type;
+        }
+    },
+    mounted(){
+        let vue = this;
+        EventBus.$on('msg-event', (message, type="success")=>{
+            vue.message = message;
+            vue.type = type;
+            vue.show = true;
+            setTimeout(() => {
+                vue.show = false;
+            }, 2000);
+        });
+    }
+}
+</script>
+
+<style scoped>
+    .info{
+        position: absolute;
+        top: 5px;
+        right: 0;
+        z-index: 20;
+    }
+
+    .fade-enter-active, .fade-leave-active{
+        transition: opacity 0.2s ease-out;
+    }
+
+    .fade-enter, .fade-leave-to{
+        opacity: 0;
+    }
+</style>

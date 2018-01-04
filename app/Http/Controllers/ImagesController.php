4f237627fe772f8c742dbcb8b71b6a072d5e713c
bg81lack <miclack30@gmail.com>
2018-01-04 17:07:20 +0100

    remove vorstand image

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -7,17 +7,41 @@ use App\Text;
 use App\User;
 use App\Event;
 use Illuminate\Http\Request;
-use App\Traits\StoreImageTrait;
 
 class ImagesController extends Controller
 {
-    use StoreImageTrait;
-
     public function pictures()
     {
         $this->authorize('can_access_pictures', User::class);
 
         return view('admin.sites.images.images');
+    }
+
+    public function uploud_group_image(Request $request)
+    {
+        $this->authorize('can_access_pictures', User::class);
+        $this->validate($request, [
+            'file' => 'required|image|mimes:jpeg,png,jpg,JPG,PNG,JPEG',
+        ]);
+
+        $paths = $this->store_image($request, 'images/', true);
+        if ($paths) {
+            $text = Text::where('id', '1')->first();
+            $text->images()->where('image_type', 'App\Text')->delete();
+            $text->images()->create([
+                'path' => $paths['main'],
+                'thump' => $paths['thump'],
+            ]);
+
+            $text->update([
+                'html' => '<div class="text-center"><img src="' . asset($paths['main']) . '" class="img-fluid" style="max-height: 50vh"></div>'
+            ]);
+            session()->flash('group_image', 'Bild wurde hinzugefügt.');
+
+            return back();
+        } else {
+            return response('No a valid file', 501);
+        }
     }
 
     public function remove_group_image()
@@ -26,15 +50,6 @@ class ImagesController extends Controller
         Text::find(1)->update([
             'html' => ''
         ]);
-        session()->flash('group_image', 'Bild wurde entfernt.');
-
-        return back();
-    }
-
-    public function remove_vorstand_image()
-    {
-        $this->authorize('can_access_pictures', User::class);
-        Text::find(2)->images()->delete();
         session()->flash('group_image', 'Bild wurde entfernt.');
 
         return back();
@@ -68,55 +83,5 @@ class ImagesController extends Controller
         $image->delete();
 
         return back();
-    }
-
-    public function uploud_group_image(Request $request)
-    {
-        $this->authorize('can_access_pictures', User::class);
-        $this->validate($request, [
-            'file' => 'required|image|mimes:jpeg,png,jpg,JPG,PNG,JPEG',
-        ]);
-
-        $paths = $this->store_image($request, 'images/', true);
-        if ($paths) {
-            $text = Text::where('id', '1')->first();
-            $text->images()->where('image_type', 'App\Text')->delete();
-            $text->images()->create([
-                'path' => $paths['main'],
-                'thump' => $paths['thump'],
-            ]);
-
-            $text->update([
-                'html' => '<div class="text-center"><img src="' . asset($paths['main']) . '" class="img-fluid" style="max-height: 50vh"></div>'
-            ]);
-            session()->flash('group_image', 'Bild wurde hinzugefügt.');
-
-            return back();
-        } else {
-            return response('No a valid file', 501);
-        }
-    }
-
-    public function uploud_vorstand_image(Request $request)
-    {
-        $this->authorize('can_access_pictures', User::class);
-        $this->validate($request, [
-            'file' => 'required|image|mimes:jpeg,png,jpg,JPG,PNG,JPEG',
-        ]);
-
-        $paths = $this->store_image($request, 'images/');
-        if ($paths) {
-            $text = Text::where('id', '2')->first();
-            $text->images()->where('image_type', 'App\Text')->delete();
-            $text->images()->create([
-                'path' => $paths['main'],
-                'thump' => $paths['thump'],
-            ]);
-            session()->flash('group_image', 'Bild wurde hinzugefügt.');
-
-            return back();
-        } else {
-            return response('No a valid file', 501);
-        }
     }
 }

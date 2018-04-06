0ca671120ab467ea2070fe1436e4e9d3757d369a
bg81lack <miclack30@gmail.com>
2018-04-06 17:42:40 +0200

    remove commended section

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,7 @@
 require('./bootstrap');
 /* global vue */
 window.Vue = require('vue');
-// import fontawesome from '@fortawesome/fontawesome';
-// import solid from '@fortawesome/fontawesome-free-solid';
-// import brands from '@fortawesome/fontawesome-free-brands';
-// fontawesome.library.add(solid);
-// fontawesome.library.add(brands);
+
 Vue.component('personen-liste', require('./components/PeopleList.vue'));
 const app = new Vue({
     el: '#app',
